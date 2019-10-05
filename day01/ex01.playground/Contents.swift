861b35b11d09a57b0a8438592cecfe97c7903997
TJOEUN-304 <tjoeun-304@TJOEUN-304ui-iMac.local>
2019-10-05 12:02:33 +0900

    .

---
+++ day01/ex01.playground/Contents.swift
@@ -0,0 +1,11 @@
+// 이것은 주석이다.
+// ios UI 기본 라이브러리 포함.
+import UIKit
+//문장의 마지막에 세미콜론은 선택적;;;;(있어도 되고, 없어도 됨)
+var str = "Hello, playground"
+
+// 기본 출력문
+print( str )
+// 아래 콘솔에 나타난다
+
+
---
+++ day01/ex02.playground/Contents.swift
@@ -0,0 +1,37 @@
+// ex02.playground
+import UIKit
+/*
+ ios앱 개발을 위한 swift4
+ 3장 변수와 상수 p47
+ 변수 : Variable - 메모리공간(4byte)
+ 상수 : Constant - 메모리공간, 값 변경 불가.
+ 
+ Swift에서 변수 var 예약어(keyword)를 사용.
+          상수 let 사용.
+ */
+
+// 변수의 선언
+// var [변수명] : 타입(자료형)
+var myStr : String = "스위프트"
+
+// 타입은 생략가능 - 타입 추정에 의해 결정됨.
+var myStr2 = "ios  앱앱앱앱"
+
+/*
+ Camel Text
+ ex) myNameLength, MyNameLength
+ 소문자로 시작 : 변수, 함수
+ 대문자로 시작 :  클래스, 구조체, 열거형, 익스텐션
+
+ *****가독성***** 중요하다!!!
+ 추천 도서 : 클린코드
+
+ 코드의 이름을 지을 때 영문법의 어순을 따른다.
+ >> 영어 : 주어 - 동사 - 목적어 gotoSchoolToday
+    한국어: 주어 - 목적어 - 동사 todaySchoolGoto
+ */
+
+
+
+
+
