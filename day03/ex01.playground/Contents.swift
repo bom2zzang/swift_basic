b9d3a3b7aedcde72db610fe5e9b21dd1f7060d11
bom2zzang <bom2zzang@me.com>
2019-10-12 10:14:25 +0900

    ,

---
+++ day03/ex01.playground/Contents.swift
@@ -0,0 +1,16 @@
+//day03
+import UIKit
+
+/*
+ 배열
+ 딕셔너리
+ 집합
+ day02  참고
+ */
+
+// 집합 Set : 데이터의 중복을 허용하지 않음.
+var mySet : Set<Int> = Set<Int>()
+mySet.insert(10)
+mySet.insert(20)
+mySet.insert(30)
+print(mySet)
