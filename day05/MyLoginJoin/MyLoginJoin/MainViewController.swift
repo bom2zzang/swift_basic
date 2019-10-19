9f1586d34c4d3fcbb2d37b9ad0c9264c0497fc8c
bom2zzang <bom2zzang@me.com>
2019-10-19 15:21:54 +0900

    goodLogin

--- day05/MyLoginJoin/MyLoginJoin/MainViewController.swift
+++ day05/MyLoginJoin/MyLoginJoin/MainViewController.swift
@@ -13,7 +13,21 @@ class MainViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         //뷰가 화면에 다시 보일 때
         
+        if appDele.isLogin == true {
+            labelStatus.text = "로그인됨\(appDele.userInfo.id)"
+        }else{
+            labelStatus.text = "로그오프상태"
+        }
     }
+    
+    
+    @IBAction func onBtnLogoff(_ sender: UIButton) {
+        
+        appDele.isLogin = false
+        appDele.userInfo = UserInfo()
+        labelStatus.text = "로그오프상태"
+    }
+    
 
 
 }
