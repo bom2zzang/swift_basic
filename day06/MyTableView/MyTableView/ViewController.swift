7a88dd6fae17497226fe69c681676e647d7fef6c
bom2zzang <bom2zzang@me.com>
2019-10-20 11:50:21 +0900

    startTableView

---
+++ day06/MyTableView/MyTableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyTableView
+//
+//  Created by TJOEUN-304 on 2019/10/20.
+//  Copyright © 2019 bombom. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
