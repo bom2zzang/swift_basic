0f4c732888a0eb0bcf6247201f40492bf997e67c
bom2zzang <bom2zzang@me.com>
2019-10-20 11:25:51 +0900

    layout

---
+++ day06/MyAutoLayout/MyAutoLayout/ViewController.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+/*
+
+ 화면 레이아웃 배치 방법
+ 1. 오토 리사이징(Auto Resizing) : 이전방법(간단한화면, 비추)
+ 2. 오토 레이아웃(Auto Layout) : 현재 주로 사용(추천)
+ 3. SwiftUI : XCODE11에서 새로 발표 됨.
+
+ */
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
